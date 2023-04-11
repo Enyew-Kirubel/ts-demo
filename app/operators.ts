18a6ffa099c8ac6fdccbdfedbeec9b5012666114
Kirubel Enyew <kirubel@addissoftware.com>
2023-04-11 23:48:48 +0300

    adds more type operator examples

--- app/operators.ts
+++ app/operators.ts
@@ -1,7 +1,26 @@
 //  keyof
-interface someRandomObject {
+interface SomeRandomObject {
     propJuan: string;
     propToo: number;
 }
 
-type KeyofRandomObject = keyof someRandomObject // "propJuan" | "propToo"
+type KeyofRandomObject = keyof SomeRandomObject // "propJuan" | "propToo"
+
+// Indexed access
+type IndexAccessRandomObject = SomeRandomObject['propJuan' | 'propToo']
+
+type Indexes = "propJuan" | 'propToo'
+type IndexAccess2 = SomeRandomObject[Indexes]
+
+type IndexAccess3 = SomeRandomObject[keyof SomeRandomObject]
+
+// const index = "propJuan"
+// type InvalidIndexAccess = SomeRandomObject[index]
+
+// mapped types
+
+// type PartialRadom = Partial<SomeRandomObject>
+
+type WhatPartialDoes<Type> = {
+    [property in keyof Type]?: Type[property]
+}
