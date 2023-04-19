6f0a12b9c6f0c4e38b3c07b0b2ea666d3da1c7e3
Kirubel <kirubel@addissoftware.com>
2023-04-19 11:54:48 +0300

    type compatability fixes

--- transpiled/index.js
+++ transpiled/index.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,13 +8,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-const express = require("express");
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
-app.get("/", (req, res) => {
+app.get("/", (request, res) => {
+    const req = request;
     switch (req.data.type) {
         case "type1":
             console.log(req.data.otherData1);
@@ -27,13 +33,14 @@ app.get("/", (req, res) => {
     }
     res.send("Hello World!");
 });
-app.post('/', (req, res) => __awaiter(this, void 0, void 0, function* () {
+app.post("/", (request, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const req = request;
     const value = yield new Promise((resolve, reject) => {
         if (req.data.type === "type1") {
             return resolve(req.data);
         }
         else {
-            return reject(new Error('not typeWan'));
+            return reject(new Error("not typeWan"));
         }
     });
     return res.send(value);
